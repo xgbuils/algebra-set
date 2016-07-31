d1e454a2937d3177ebe408b5c8d122c001e6d489
Xavier Garcia Buils <xgbuils@gmail.com>
2016-07-31 22:05:46 +0200

    add better test for interval-cast.js

--- test/type-casting/to-interval/index_test.js
+++ test/type-casting/to-interval/index_test.js
@@ -32,4 +32,30 @@ describe('toInterval', function () {
       expect(raw(interval)).to.be.deep.equal(samples['[3, 9)'])
     })
   })
+
+  describe('when is Interval instance', function () {
+    var param
+    var result
+    beforeEach(function () {
+      param = Interval.create(samples['[4, 5]'])
+      result = toInterval(param)
+    })
+    it('returns an Interval instance', function () {
+      expect(result).to.be.instanceOf(Interval)
+    })
+    it('element returned wraps a correct raw interval', function () {
+      expect(raw(result)).to.be.deep.equal(samples['[4, 5]'])
+    })
+    it('interval passed and interval returned does not have the same reference', function () {
+      expect(result).to.not.be.equal(param)
+    })
+  })
+
+  describe('when type of value is impossible to cast', function () {
+    it('param is the same reference to result', function () {
+      var param = {foo: 'bar'}
+      var result = toInterval(param)
+      expect(result).to.be.equal(param)
+    })
+  })
 })
--- test/type-casting/to-multi-interval/interval-cast_test.js
+++ test/type-casting/to-multi-interval/interval-cast_test.js
@@ -7,7 +7,7 @@ var Interval = require('../../../src/interval.js')
 var intervalCast = require('../../../src/type-casting/to-multi-interval/interval-cast')(Interval)
 
 describe('intervalCast', function () {
-  describe('when is array of raw interval', function () {
+  describe('when param is some type castable to interval', function () {
     var interval
     beforeEach(function () {
       interval = intervalCast(samples['[3, 5)'])
@@ -21,6 +21,51 @@ describe('intervalCast', function () {
     })
     it('first element wraps the raw interval', function () {
       expect(raw(interval[0])).to.be.deep.equal(samples['[3, 5)'])
+    })
+  })
+
+  describe('when param is an array of castable-to-interval values', function () {
+    var param
+    var result
+    beforeEach(function () {
+      param = [
+        '(4, 8)',
+        samples['[4, 5]'],
+        new Interval('{-1}')
+      ]
+      result = intervalCast(param)
+    })
+    it('returns an array with the same elements', function () {
+      expect(result).to.be.an('array')
+      expect(result.length).to.be.equal(param.length)
+    })
+    it('first element is an Interval instance', function () {
+      expect(result.every(function (e) {
+        return e instanceof Interval
+      })).to.be.equal(true)
+    })
+    it('first element wraps a correct raw interval', function () {
+      expect(result.map(raw)).to.be.deep.equal([
+        samples['(4, 8)'],
+        samples['[4, 5]'],
+        samples['{-1}']
+      ])
+    })
+  })
+
+  describe('when param is a array with a no castable type', function () {
+    it('param is the same reference to result', function () {
+      var param = ['[2, 5)', new RegExp('^x+c', 'i')]
+      var result = intervalCast(param)
+      expect(result).to.be.equal(param)
+    })
+  })
+
+  describe('when param is a array with a no castable type', function () {
+    it('param is the same reference to result', function () {
+      var param = new RegExp('a*$', 'g')
+      var result = intervalCast(param)
+      expect(result).to.be.equal(param)
     })
   })
 })
