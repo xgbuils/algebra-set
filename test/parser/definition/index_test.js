8f1a6c8ecc080cefab1bba4c84d575f7eff5b7c9
Xavier Garcia Buils <xgbuils@gmail.com>
2016-12-02 00:35:15 +0100

    remove lexer dependencies in definition parser tests

--- test/parser/definition/index_test.js
+++ test/parser/definition/index_test.js
@@ -4,14 +4,19 @@ var MSet = require('math.set')
 var rawSet = require('math.set/src/raw-set')
 var MFunction = require('../../../src/')
 
-var ExpressionTokenBuilder = require('../../../src/lexer/token-builder/expression-token-builder.js')
-var NumberTokenBuilder = require('../../../src/lexer/token-builder/number-token-builder.js')
-var SymbolTokenBuilder = require('../../../src/lexer/token-builder/symbol-token-builder.js')
+var Iterum = require('iterum')
+var List = Iterum.List
 
-var TokenCalculator = require('../../../src/lexer/token-calculator')
-
-var lexer = require('../../../src/lexer/definition/')
 var parser = require('../../../src/parser/definition/')
+
+function createToken (value, type, column, key) {
+    return {
+        value: value,
+        type: type,
+        column: column,
+        key: key
+    }
+}
 
 var sumFn = function (a, b) {
     return [{
@@ -24,23 +29,14 @@ var sumFn = function (a, b) {
 }
 
 describe('parser/definition', function () {
-    function createTokenCalculator (functions, sets) {
-        return new TokenCalculator([
-            new ExpressionTokenBuilder(functions, 'function'),
-            new ExpressionTokenBuilder(sets, 'set'),
-            new NumberTokenBuilder(),
-            new SymbolTokenBuilder()
-        ])
-    }
     describe('given simple variable', function () {
         it('returns the set that means', function () {
-            var string = 'a'
             var a = MSet('(2, 3)')
-            var functions = {}
-            var sets = {
-                a: a
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // a
+            var lex = List([
+                createToken(a, 'set')
+            ])
             var result = parser(lex.build())
             expect(result).to.be.deep.equal(a)
         })
@@ -48,18 +44,19 @@ describe('parser/definition', function () {
 
     describe('given simple function expression', function () {
         it('returns the set that means', function () {
-            var string = 'sum(a, b)'
             var a = MSet('(2, 3)')
             var b = MSet('[1, 4)')
             var sum = MFunction(sumFn)
-            var functions = {
-                sum: sum
-            }
-            var sets = {
-                a: a,
-                b: b
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // sum(a, b)
+            var lex = List([
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(a, 'set'),
+                createToken(',', ','),
+                createToken(b, 'set'),
+                createToken(')', ')')
+            ])
             var result = parser(lex.build())
             var expected = MSet('(3, 7)')
             expect(rawSet(result)).to.be.deep.equal(rawSet(expected))
@@ -68,15 +65,17 @@ describe('parser/definition', function () {
 
     describe('given simple tuple expression', function () {
         it('returns the tuple that means', function () {
-            var string = '(a, b)'
             var a = MSet('(2, 3)')
             var b = MSet('[1, 4)')
-            var functions = {}
-            var sets = {
-                a: a,
-                b: b
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // (a, b)
+            var lex = List([
+                createToken('(', '('),
+                createToken(a, 'set'),
+                createToken(',', ','),
+                createToken(b, 'set'),
+                createToken(')', ')')
+            ])
             var result = parser(lex.build())
             expect(result.map(rawSet)).to.be.deep.equal([rawSet(a), rawSet(b)])
         })
@@ -84,64 +83,82 @@ describe('parser/definition', function () {
 
     describe('given function with number of parameters greater than correct', function () {
         it('throws an error', function () {
+            var column = 7
+            var sumKey = 'sum'
             function test () {
-                var string = 'sum(a, b, c)'
                 var a = MSet('(2, 3)')
                 var b = MSet('[1, 4)')
                 var c = MSet('(3, 5]')
                 var sum = MFunction(sumFn)
-                var functions = {
-                    sum: sum
-                }
-                var sets = {
-                    a: a,
-                    b: b,
-                    c: c
-                }
-                var lex = lexer(string, createTokenCalculator(functions, sets))
+
+                // sum(a, b, c)
+                var lex = List([
+                    createToken(sum, 'function', 1, sumKey),
+                    createToken('(', '('),
+                    createToken(a, 'set'),
+                    createToken(',', ','),
+                    createToken(b, 'set'),
+                    createToken(',', ',', column, ','),
+                    createToken(c, 'set'),
+                    createToken(')', ')')
+                ])
                 parser(lex.build())
             }
 
-            expect(test).to.throw('Expected token `,` instead `)` in column 9. `sum` arity must be 2.')
+            expect(test).to.throw('Expected token `,` instead `)` in column ' +
+                column + '. `' + sumKey + '` arity must be 2.')
         })
     })
 
     describe('given function with number of parameters less than correct', function () {
         it('throws an error', function () {
+            var column = 19
+            var sumKey = 'sum'
             function test () {
-                var string = 'sum(a)'
                 var a = MSet('(2, 3)')
                 var sum = MFunction(sumFn)
-                var functions = {
-                    sum: sum
-                }
-                var sets = {
-                    a: a
-                }
-                var lex = lexer(string, createTokenCalculator(functions, sets))
+
+                // sum(a)
+                var lex = List([
+                    createToken(sum, 'function', 2, sumKey),
+                    createToken('(', '('),
+                    createToken(a, 'set'),
+                    createToken(')', ')', column, ')')
+                ])
                 parser(lex.build())
             }
 
-            expect(test).to.throw('Expected token `,` instead `)` in column 6. `sum` arity must be 2.')
+            expect(test).to.throw('Expected token `,` instead `)` in column ' +
+                column + '. `' + sumKey + '` arity must be 2.')
         })
     })
 
     describe('given nested function expression', function () {
         it('returns the set that means', function () {
-            var string = 'sum(sum(a, sum(b, 8)), c)'
             var a = MSet('[2, 3)')
             var b = MSet('[1, 4)')
             var c = MSet('[3, 5]')
             var sum = MFunction(sumFn)
-            var functions = {
-                sum: sum
-            }
-            var sets = {
-                a: a,
-                b: b,
-                c: c
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // sum(sum(a, sum(b, 8)), c)
+            var lex = List([
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(a, 'set'),
+                createToken(',', ','),
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(b, 'set'),
+                createToken(',', ','),
+                createToken(new MSet('{8}'), 'set'),
+                createToken(')', ')'),
+                createToken(')', ')'),
+                createToken(',', ','),
+                createToken(c, 'set'),
+                createToken(')', ')')
+            ])
             var result = parser(lex.build())
             var expected = MSet('[14, 20)')
             expect(rawSet(result)).to.be.deep.equal(rawSet(expected))
@@ -150,20 +167,26 @@ describe('parser/definition', function () {
 
     describe('given nested tuples expression', function () {
         it('returns the tuple that means', function () {
-            var string = '((a, (b, a)), c)'
             var a = MSet('[2, 3)')
             var b = MSet('[1, 4)')
             var c = MSet('[3, 5]')
-            var sum = MFunction(sumFn)
-            var functions = {
-                sum: sum
-            }
-            var sets = {
-                a: a,
-                b: b,
-                c: c
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // ((a, (b, a)), c)
+            var lex = List([
+                createToken('(', '('),
+                createToken('(', '('),
+                createToken(a, 'set'),
+                createToken(',', ','),
+                createToken('(', '('),
+                createToken(b, 'set'),
+                createToken(',', ','),
+                createToken(a, 'set'),
+                createToken(')', ')'),
+                createToken(')', ')'),
+                createToken(',', ','),
+                createToken(c, 'set'),
+                createToken(')', ')')
+            ])
             var result = parser(lex.build())
             expect(result).to.be.deep.equal([[a, [b, a]], c])
         })
@@ -171,20 +194,33 @@ describe('parser/definition', function () {
 
     describe('given nested mixed expressions (tuples and functions)', function () {
         it('returns the tuple that means', function () {
-            var string = '((a, sum(b, a)), sum(c, b))'
             var a = MSet('[2, 3)')
             var b = MSet('[1, 4)')
             var c = MSet('[3, 5]')
             var sum = MFunction(sumFn)
-            var functions = {
-                sum: sum
-            }
-            var sets = {
-                a: a,
-                b: b,
-                c: c
-            }
-            var lex = lexer(string, createTokenCalculator(functions, sets))
+
+            // ((a, sum(b, a)), sum(c, b))
+            var lex = List([
+                createToken('(', '('),
+                createToken('(', '('),
+                createToken(a, 'set'),
+                createToken(',', ','),
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(b, 'set'),
+                createToken(',', ','),
+                createToken(a, 'set'),
+                createToken(')', ')'),
+                createToken(')', ')'),
+                createToken(',', ','),
+                createToken(sum, 'function'),
+                createToken('(', '('),
+                createToken(c, 'set'),
+                createToken(',', ','),
+                createToken(b, 'set'),
+                createToken(')', ')'),
+                createToken(')', ')')
+            ])
             var result = parser(lex.build())
             var first = result[0]
             var second = result[1]
