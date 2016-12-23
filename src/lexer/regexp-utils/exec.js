e8326f9db6363d4b67668f2c7fda7c8266a6aced
Xavier Garcia Buils <xgbuils@gmail.com>
2016-12-24 00:42:10 +0100

    remove code of lexers and use parser.generator 0.2.0 version

--- src/lexer/regexp-utils/exec.js
+++
@@ -1,8 +0,0 @@
-function exec (regexp, string, column) {
-    regexp.lastIndex = column
-    var matches = regexp.exec(string)
-    var match = matches && matches[0]
-    return match && regexp.lastIndex - column === match.length ? match : null
-}
-
-module.exports = exec
--- test/lexer/repeat.js
+++
@@ -1,9 +0,0 @@
-function repeat (num, ch) {
-    var str = ''
-    for (var i = 0; i < num; ++i) {
-        str += ch
-    }
-    return str
-}
-
-module.exports = repeat
